eae3ea04727ab55a34c52f322a5fb91d66664a8b
alibhojani <ali.bhojani@mail.mcgill.ca>
2016-01-30 22:08:16 -0500

    Basic Player Movement

---
+++ Deep Under/Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private float speed = 20f;
+	private float turnSpeed = 60f;
+	Rigidbody rigidbody;
+	public Camera camera; 
+
+	float h; 
+	float v; 
+	
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>(); 
+
+	}
+
+	void FixedUpdate () {
+		h = Input.GetAxisRaw("Horizontal");
+		v = Input.GetAxisRaw("Vertical");	
+		Move(h,v);
+		Turn();
+	}
+
+	//TODO: movement is jittery when button first pressed; fix. 
+	private void Move (float h, float v) { 
+		Vector3 movementHorizontal = (transform.right * h) * speed * Time.deltaTime;
+		Vector3 movementVertical = (transform.forward * v) * speed * Time.deltaTime; 
+		rigidbody.MovePosition(transform.position + movementHorizontal + movementVertical);
+	}
+
+	private void Turn () { 
+		if (v!=0) { 
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, camera.transform.rotation, turnSpeed * Time.deltaTime );
+		}
+	}
+}
---
+++ Deep Under/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	Vector3 offset; 
+	public GameObject player; 
+	float cameraSpeed = 120f;
+	
+	void Start () {
+		offset = transform.position - player.transform.position;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false; 
+	}
+
+	void FixedUpdate () {
+
+		Vector3 oldRot = transform.rotation * Vector3.forward; //get current/old rotation in vector form
+		transform.Rotate(Vector3.up * Input.GetAxis ("Mouse X") * cameraSpeed * Time.deltaTime); //rotate camera on Y Axis according to MouseX
+		transform.Rotate(Vector2.right * Input.GetAxis ("Mouse Y") * cameraSpeed * Time.deltaTime); //rotate camera on X axis according to MouseY
+		transform.rotation = Quaternion.Euler(new Vector3 (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f));
+		Vector3 newRot = transform.rotation * Vector3.forward; // save new rotation in vector form 
+		Quaternion change = Quaternion.FromToRotation(oldRot, newRot); //record change in rotation using the 2 vectors
+		offset = change * offset; //apply change in rotation to offset
+		transform.position = player.transform.position + offset; // apply offset
+
+	}
+
+		
+
+}
