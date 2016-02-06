5473de18781f87e8b5909e5a6a76f50b965e4f05
alibhojani <ali.bhojani@mail.mcgill.ca>
2016-02-06 00:55:08 -0500

    Misc (want to pull)

--- Deep Under/Assets/Scripts/CameraFollow.cs
+++ Deep Under/Assets/Scripts/CameraFollow.cs
@@ -6,7 +6,7 @@ public class CameraFollow : MonoBehaviour {
 	Vector3 offset; 
 	public Player player;  
 	private float cameraSpeed = 60f;
-	private float smoothing = 5f; 
+	private float smoothing = 15f; 
 	
 	void Start () {
 		//dplayer = GetComponent<Player>();
@@ -18,13 +18,15 @@ public class CameraFollow : MonoBehaviour {
 	void FixedUpdate () {
 		
 		Vector3 oldRot = transform.rotation * Vector3.forward; //get current/old rotation in vector form
-		transform.Rotate(Vector3.up * Input.GetAxis ("Mouse X") * cameraSpeed * Time.deltaTime); //rotate camera on Y Axis according to MouseX
-		transform.Rotate(Vector2.right * Input.GetAxis ("Mouse Y") * cameraSpeed * Time.deltaTime); //rotate camera on X axis according to MouseY
+		if (Mathf.Abs(Input.GetAxis("Mouse X")) > 0.1f)transform.Rotate(Vector3.up * Input.GetAxis ("Mouse X") * cameraSpeed * Time.deltaTime); //rotate camera on Y Axis according to MouseX
+		if (Mathf.Abs(Input.GetAxis("Mouse Y")) > 0.1f)transform.Rotate(Vector2.right * Input.GetAxis ("Mouse Y") * cameraSpeed * Time.deltaTime); //rotate camera on X axis according to MouseY
 		transform.rotation = Quaternion.Euler(new Vector3 (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f));
 		Vector3 newRot = transform.rotation * Vector3.forward; // save new rotation in vector form 
 		Quaternion change = Quaternion.FromToRotation(oldRot, newRot); //record change in rotation using the 2 vectors
 		offset = change * offset; //apply change in rotation to offset
 		Vector3 targetPos = player.transform.position + offset; // apply offset
+
+
 		if (Vector3.Distance(transform.position, targetPos) > 0.1f)transform.position = Vector3.Lerp(transform.position, targetPos,(1 - Mathf.Exp( -smoothing * Time.deltaTime ))); //TODO: jitter caused by smoothing.
 		//transform.position = targetPos;
 	}
--- Deep Under/Assets/Scripts/Player.cs
+++ Deep Under/Assets/Scripts/Player.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	private float speed = 20f;
+	//private float lsXDeadValue = 0.058f; //fix dead values in both LS and RS
+	//private float lsYDeadValue = 1f;	//fix dead value
+
+	private float speed = 40f;
 	//private float turnSpeed = 60f;
 	Rigidbody rigidbody;
 	public Camera camera; 
 
-	private float h; 
-	private float v; 
-	private float u;
+
+	public float h; 
+	public float v; 
+	public float u;
 	
 	// Use this for initialization
 	void Start () {
@@ -19,31 +23,43 @@ public class Player : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		h = Input.GetAxisRaw("Horizontal");
-		v = Input.GetAxisRaw("Vertical");	
-		if (Input.GetKey(KeyCode.Q)) u = 1;
-		else if (Input.GetKey(KeyCode.E)) u = -1;
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");	
+		if (Input.GetKey (KeyCode.Joystick1Button4)) u = 1;
+		else if (Input.GetKey (KeyCode.Joystick1Button5)) u = -1;
 		else u = 0;
 		Move(h,v,u);
 		autoTurn();
-		bob();
+		//bob();
+		controllerButtonTest(); 
 	}
 
 	private void Move (float h, float v, float u) { 
-		Vector3 movementHorizontal = (camera.transform.right * h) * speed/4f * Time.deltaTime;
+		//Debug.Log(u);
+		Vector3 movementHorizontal = (camera.transform.right * h) * speed/2f * Time.deltaTime;
 		Vector3 movementForward = (camera.transform.forward * v) * speed * Time.deltaTime; 
-		Vector3 movementVertical = (transform.up * u) * speed/4f * Time.deltaTime;
-		rigidbody.MovePosition(transform.position + movementVertical + movementHorizontal + movementForward);
+		Vector3 movementVertical = (transform.up * u) * speed/2f * Time.deltaTime;
+		Debug.Log(movementVertical.ToString());
+		rigidbody.MovePosition(transform.position + movementHorizontal + movementForward +  movementVertical );
 	}
 
 
 	private void autoTurn () { 
-		if (v!=0) transform.rotation = Quaternion.RotateTowards (transform.rotation, camera.transform.rotation, 2f);	
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, camera.transform.rotation, 2f);	
 	}
 
 	private void bob () { 
-		if (v == 0 && h == 0) { 
+		if (v < 1f && h < 1f) { 
 			rigidbody.MovePosition(transform.position + (Vector3.up * (Mathf.Sin(Time.time * 2f)) * 0.005f));
 		}
+	}
+
+	private void controllerButtonTest() { 
+		/*if (Input.GetKeyDown (KeyCode.Joystick1Button0)) Debug.Log("Square pressed");
+		if (Input.GetKeyDown (KeyCode.Joystick1Button1)) Debug.Log("X pressed");
+		if (Input.GetKeyDown (KeyCode.Joystick1Button2)) Debug.Log("O pressed");
+		if (Input.GetKeyDown (KeyCode.Joystick1Button3)) Debug.Log("Triangle pressed");
+		if (Input.GetKeyDown (KeyCode.Joystick1Button4)) Debug.Log("4");
+		if (Input.GetKeyDown (KeyCode.Joystick1Button5)) Debug.Log("5");*/
 	}
 }
