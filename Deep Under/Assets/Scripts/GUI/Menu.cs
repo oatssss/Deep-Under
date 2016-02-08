051dab338bb027133ab9b47021b73c27d906a813
Othniel Cundangan <othniel.cundangan@gmail.com>
2016-02-08 02:16:09 -0500

    GUI functional.

---
+++ Deep Under/Assets/Scripts/GUI/Menu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    [SerializeField] private Animator animator;
+    private Animator Animator { get { return this.animator; } }
+    [SerializeField] private CanvasGroup CanvasGroup;
+    [SerializeField] private RectTransform RectTransform;
+    
+    public void Open()
+    {
+        this.Activate();
+        this.Animator.SetTrigger("Open");
+    }
+    
+    public void Close()
+    {
+        this.Deactivate();
+        this.Animator.SetTrigger("Close");
+    }
+    
+    public void Hide()
+    {
+        this.Deactivate();
+        if (GUIManager.Instance.PreviousMenu() == this)
+            { this.Animator.SetTrigger("Hide"); }
+        else
+            { this.Animator.SetTrigger("HidePermanent"); }
+    }
+    
+    public void Reset()
+    {
+        this.Deactivate();
+        this.Animator.SetTrigger("Reset");
+    }
+    
+    void Awake()
+    {
+        this.RectTransform.offsetMin = this.RectTransform.offsetMax = Vector2.zero;
+    }
+    
+    private void Activate()
+    {
+        this.CanvasGroup.blocksRaycasts = this.CanvasGroup.interactable = true;
+    }
+    
+    private void Deactivate()
+    {
+        this.CanvasGroup.blocksRaycasts = this.CanvasGroup.interactable = false;
+    }
+}
