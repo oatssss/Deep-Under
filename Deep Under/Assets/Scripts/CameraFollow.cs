0484718274e78c541edbe1a5ef75d539191cdbbd
alibhojani <ali.bhojani@mail.mcgill.ca>
2016-02-13 21:56:49 -0500

    energy pods + improved movement (no lag)

--- Deep Under/Assets/Scripts/CameraFollow.cs
+++ Deep Under/Assets/Scripts/CameraFollow.cs
@@ -7,50 +7,68 @@ public class CameraFollow : MonoBehaviour {
 	public Player player;  
 	private float cameraSpeed = 60f;
 	private float smoothing = 15f; 
-	
+	public float rollSpeed =  3f;
+
+	private float x;
+	private float y; 
+	private float maxLookUp = 80f;
+	private float maxLookDown = 70f;
+	public float startLook = 0f; 
+
+
 	void Start () {
 		//dplayer = GetComponent<Player>();
 		offset = transform.position - player.transform.position;
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false; 
 
+
 	}
 
 	void FixedUpdate () {
-		
+		//Debug.Log(startLook);
 		Vector3 oldRot = transform.rotation * Vector3.forward; //get current/old rotation in vector form
-		if (Mathf.Abs(Input.GetAxis("Mouse X")) > 0.1f)transform.RotateAround(transform.position, transform.up, Input.GetAxis ("Mouse X") * cameraSpeed * Time.deltaTime); //rotate camera on Y Axis according to MouseX
-		if (Mathf.Abs(Input.GetAxis("Mouse Y")) > 0.1f)transform.RotateAround(transform.position, transform.right, Input.GetAxis ("Mouse Y") * cameraSpeed * Time.deltaTime); //rotate camera on X axis according to MouseY
-		//transform.rotation = Quaternion.Euler(new Vector3 (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f));
-		fixRoll();
-		Debug.Log(transform.rotation.eulerAngles.z);
+		y = Input.GetAxis("Mouse X"); //rotation on y axis
+		x = Input.GetAxis("Mouse Y"); //rotation on x axis
+		cameraRotate(x,y);
 		Vector3 newRot = transform.rotation * Vector3.forward; // save new rotation in vector form 
 		Quaternion change = Quaternion.FromToRotation(oldRot, newRot); //record change in rotation using the 2 vectors
 		offset = change * offset; //apply change in rotation to offset
 		Vector3 targetPos = player.transform.position + offset; // apply offset
-		if (Vector3.Distance(transform.position, targetPos) > 0.1f)transform.position = Vector3.Lerp(transform.position, targetPos,(1 - Mathf.Exp( -smoothing * Time.deltaTime ))); //TODO: jitter caused by smoothing.
-		//transform.position = targetPos;
+		//if (Vector3.Distance(transform.position, targetPos) > 0.1f) transform.position = Vector3.Lerp(transform.position, targetPos,(1 - Mathf.Exp( -smoothing * Time.deltaTime ))); //TODO: jitter caused by smoothing.
+		transform.position = targetPos;
 	}
 
-	private Vector3 SmoothApproach( Vector3 pastPosition, Vector3 pastTargetPosition, Vector3 targetPosition, float speed )
-    {
-        float t = Time.deltaTime * speed;
-        Vector3 v = ( targetPosition - pastTargetPosition ) / t;
-        Vector3 f = pastPosition - pastTargetPosition + v;
-        return targetPosition - v + f * Mathf.Exp( -t );
+    
+
+    private void cameraRotate (float x, float y) { 
+		transform.RotateAround(transform.position, transform.up, y * cameraSpeed * Time.deltaTime); 
+		if (x < 0) { 
+			if (startLook < maxLookUp) { 
+				startLook -= x * cameraSpeed * Time.deltaTime;
+				transform.RotateAround(transform.position, transform.right, x * cameraSpeed * Time.deltaTime);
+			}
+		}
+		else if (x > 0) { 
+			if (startLook > -maxLookDown) { 
+				startLook -= x * cameraSpeed * Time.deltaTime;
+				transform.RotateAround(transform.position, transform.right, x * cameraSpeed * Time.deltaTime);
+			}
+		}
+		transform.rotation = Quaternion.Euler(new Vector3 (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f));
     }
 
+	//OBSOLETE
     private void fixRoll(){
     	if (transform.rotation.eulerAngles.z > 2f && transform.rotation.eulerAngles.z < 358f) {
 	    	if (transform.rotation.eulerAngles.z > 180f) { 
-	    		transform.RotateAround(transform.position, transform.forward, Time.deltaTime * cameraSpeed/2f );
+	    		transform.RotateAround(transform.position, transform.forward, Time.deltaTime * cameraSpeed * rollSpeed);
 	    	}
 	    	else {
-	    	transform.RotateAround(transform.position, transform.forward, -Time.deltaTime * cameraSpeed/2f );
+	    	transform.RotateAround(transform.position, transform.forward, -Time.deltaTime * cameraSpeed * rollSpeed);
 	    	}
 		}
     }
-
 		
 
 }
---
+++ Deep Under/Assets/pod.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pod : MonoBehaviour {
+
+	private float rotSpeed = 40f; 
+	public float orbStrength = 20f; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotate(); 
+	}
+
+	private void rotate () { 
+		transform.Rotate(new Vector3 (0f, rotSpeed * Time.deltaTime, 0f));
+		transform.Rotate(new Vector3 (rotSpeed * Time.deltaTime, 0f, 0f));
+	}
+
+	void OnTriggerStay(Collider other) { 
+		if (other.CompareTag("Auliv")) { 
+			Player auliv = other.GetComponent<Player>();
+			auliv.charging = true; 
+			auliv.addEnergy(this.orbStrength);
+		}
+	}
+
+	void OnTriggerExit (Collider other) { 
+		if (other.CompareTag("Auliv")) { 
+			Player auliv = other.GetComponent<Player>();
+			auliv.charging = false; 
+			//Debug.Log ("Auliv has left the building");
+		}
+	}
+}
