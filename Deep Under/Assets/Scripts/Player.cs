334ed5dbc45f75c307defc0cb92f680550f67617
Othniel Cundangan <othniel.cundangan@gmail.com>
2016-02-21 16:35:55 -0500

    Fix ascend/descend.

--- Deep Under/Assets/Scripts/Player.cs
+++ Deep Under/Assets/Scripts/Player.cs
@@ -8,98 +8,96 @@ public class Player : MonoBehaviour {
 
 	private float speed = 80f;
 	Rigidbody rigidbody;
-	public CameraFollow camera; 
+	public CameraFollow camera;
 
-	public Light spotlight; 
+	public Light spotlight;
 	private bool lightOn = false;
 
 	public Transform lightOrb;
 	public Transform lightOrbPosition;
 
-	public Transform [] LWingLights; 
+	public Transform [] LWingLights;
 	public Transform [] RWingLights;
 
-	private float h; 
-	private float v; 
-	private float u;
+	private float h;
+	private float v;
+	private float a;
 
-	public float energy; 
-	private float maxEnergy = 100f; 
+	public float energy;
+	private float maxEnergy = 100f;
 	public float energyDrainRate;
-	public bool charging = false; 
-	
+	public bool charging = false;
+
 	// Use this for initialization
 	void Start () {
 		energy = 80f;
 		rigidbody = GetComponent<Rigidbody>();
-		spotlight.gameObject.SetActive(lightOn); 
+		spotlight.gameObject.SetActive(lightOn);
 		energyDrainRate = 3f;
 	}
 
 	void FixedUpdate () {
-		
+
 		h = Input.GetAxis("Horizontal");
-		v = Input.GetAxis("Vertical");	
-		if (Input.GetKey (KeyCode.Joystick1Button4)) u = 1;
-		else if (Input.GetKey (KeyCode.Joystick1Button5)) u = -1;
-		else u = 0;
-		Move(h,v,u);
+		v = Input.GetAxis("Vertical");
+        a = Input.GetAxis("Altitude");
+		Move(h,v,a);
 		//autoTurn();
 		//bob();
 
 	}
 
-	void Update() { 
+	void Update() {
 		if (Input.GetKeyUp(KeyCode.Joystick1Button11) || Input.GetKeyUp(KeyCode.F)) lightToggle();
 		if (Input.GetKeyUp(KeyCode.Joystick1Button0) || Input.GetKeyUp(KeyCode.Space)) createLightOrb();
-		//controllerButtonTest(); 
+		//controllerButtonTest();
 		removeEnergy(energyDrainRate);
 	}
 
 	private void Move (float h, float v, float u) {
-	 
+
 		Vector3 movementHorizontal = (camera.transform.right * h) * speed * Time.deltaTime;
-		Vector3 movementForward = (camera.transform.forward * v) * speed * Time.deltaTime; 
-		Vector3 dir = movementHorizontal + movementForward; 
+		Vector3 movementForward = (camera.transform.forward * v) * speed * Time.deltaTime;
+        Vector3 movementVertical = (Vector3.up * u) * speed/2f * Time.deltaTime;
+		Vector3 dir = movementHorizontal + movementForward;
 		if (Mathf.Abs(h) >= 0.2f || Mathf.Abs(v) >= 0.2f) turn(dir);
-		Vector3 movementVertical = (transform.up * u) * speed/2f * Time.deltaTime;
 		rigidbody.MovePosition(transform.position + movementHorizontal + movementForward +  movementVertical);
 
 	}
 
 
-	private void autoTurn () { 
+	private void autoTurn () {
 		if (v > 0f && Mathf.Abs(h) < 0.5f){
-	 		transform.rotation = Quaternion.RotateTowards (transform.rotation, camera.transform.rotation, 4f);	
+	 		transform.rotation = Quaternion.RotateTowards (transform.rotation, camera.transform.rotation, 4f);
 	 	}
 	}
 
-	private void turn (Vector3 dir) { 
+	private void turn (Vector3 dir) {
 		Quaternion lookDirection = Quaternion.LookRotation(dir);
 		float turnSpeed = Mathf.Sqrt(Mathf.Pow(h,2) + Mathf.Pow(v,2)) * 4f;
-		transform.rotation = Quaternion.Lerp(transform.rotation, lookDirection, turnSpeed * Time.deltaTime);	
+		transform.rotation = Quaternion.Lerp(transform.rotation, lookDirection, turnSpeed * Time.deltaTime);
 	}
 
-	private void bob () { 
-		if (v < 1f && h < 1f) { 
+	private void bob () {
+		if (v < 1f && h < 1f) {
 			rigidbody.MovePosition(transform.position + (Vector3.up * (Mathf.Sin(Time.time * 2f)) * 0.005f));
 		}
 	}
 
-	private void lightToggle () { 
-		if (lightOn) lightOn = false; 
-		else lightOn = true; 
+	private void lightToggle () {
+		if (lightOn) lightOn = false;
+		else lightOn = true;
 		spotlight.gameObject.SetActive(lightOn);
 	}
 
-	private void createLightOrb () { 
+	private void createLightOrb () {
 		lightOrb.position = lightOrbPosition.transform.position;
 		GameObject.Instantiate(lightOrb);
 	}
 
-	public void addEnergy (float orbStrength) { 
+	public void addEnergy (float orbStrength) {
 		if (energy < maxEnergy ){
-			energy += orbStrength * Time.deltaTime; 
+			energy += orbStrength * Time.deltaTime;
 //			Debug.Log(energy);
 		}
 	}
@@ -117,14 +115,14 @@ public class Player : MonoBehaviour {
 	}
 
 	/* time decreases energy and other fishes decrease energy; rate fluctuates depeding on whether auliv is near another fish */
-	public void removeEnergy (float rate) { 
-		if (energy > 0f && !charging) { 
-			energy -= rate * Time.deltaTime; 
+	public void removeEnergy (float rate) {
+		if (energy > 0f && !charging) {
+			energy -= rate * Time.deltaTime;
 //			Debug.Log(energy);
-		}	 
+		}
 	}
 
-	private void controllerButtonTest() { 
+	private void controllerButtonTest() {
 		if (Input.GetKeyDown (KeyCode.Joystick1Button0)) Debug.Log("Square pressed");
 		if (Input.GetKeyDown (KeyCode.Joystick1Button1)) Debug.Log("X pressed");
 		if (Input.GetKeyDown (KeyCode.Joystick1Button2)) Debug.Log("O pressed");
--- Deep Under/Assets/Scripts/pod.cs
+++ Deep Under/Assets/Scripts/pod.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class pod : MonoBehaviour {
 
-	private float rotSpeed = 40f; 
-	public float orbStrength = 20f; 
+	private float rotSpeed = 40f;
+	public float orbStrength = 20f;
 
 	Player auliv;
 	// Use this for initialization
@@ -12,29 +12,29 @@ public class pod : MonoBehaviour {
 //		auliv = GameObject.Find("auliv/SUB_RIG_007").GetComponent<Player>();
 		auliv = GameObject.Find ("auliv").GetComponent<Player>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		rotate(); 
+		rotate();
 	}
 
-	private void rotate () { 
+	private void rotate () {
 		transform.Rotate(new Vector3 (0f, rotSpeed * Time.deltaTime, 0f));
 		transform.Rotate(new Vector3 (rotSpeed * Time.deltaTime, 0f, 0f));
 	}
 
-	void OnTriggerStay(Collider other) { 
-		if (other.CompareTag("Player") && auliv != null) { 
-			auliv.charging = true; 
+	void OnTriggerStay(Collider other) {
+		if (other.CompareTag("Player") && auliv != null) {
+			auliv.charging = true;
 			auliv.addEnergy(this.orbStrength);
-			Debug.Log ("Auliv is hereee");
+			// Debug.Log ("Auliv is hereee");
 		}
 	}
 
-	void OnTriggerExit (Collider other) { 
-		if (other.CompareTag("Player") && auliv != null) { 
-			auliv.charging = false; 
-			Debug.Log ("Auliv has left the building");
+	void OnTriggerExit (Collider other) {
+		if (other.CompareTag("Player") && auliv != null) {
+			auliv.charging = false;
+			// Debug.Log ("Auliv has left the building");
 		}
 	}
 }
