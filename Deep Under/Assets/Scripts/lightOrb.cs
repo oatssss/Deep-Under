8028d03bd9c68328fe0c9a06962986ab0de08737
alibhojani <ali.bhojani@mail.mcgill.ca>
2016-03-01 22:20:43 -0500

    Aiming Mode + Throw Light Orb Shift (or L2) to go into aiming mode, Left click (or R2)  to throw orb while in aiming mode

--- Deep Under/Assets/Scripts/lightOrb.cs
+++ Deep Under/Assets/Scripts/lightOrb.cs
@@ -4,14 +4,25 @@ using System.Collections;
 public class lightOrb : MonoBehaviour {
 
 	private float timer = 0f; 
-	// Use this for initialization
-	void Start () {
-	
+	public Rigidbody rb; 
+
+	void Awake() {
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		timer += Time.deltaTime; 
-		if (timer > 10f) Destroy(this.gameObject);
+		if (rb != null) { 
+			timer += Time.deltaTime; 
+			//if (timer > 10f) Destroy(this.gameObject);
+			if (timer > 1f) GameObject.Destroy(rb);
+		}
+		else bob();
 	}
+
+	private void bob () {
+		transform.position = transform.position + (Vector3.up * (Mathf.Sin(Time.time * 3f)) * 0.01f);
+	}
+
+
 }
