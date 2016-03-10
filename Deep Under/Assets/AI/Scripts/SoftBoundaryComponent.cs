858b1d833d4004690f4409e9f69bae6bfa5e139f
Othniel Cundangan <othniel.cundangan@gmail.com>
2016-03-10 12:35:22 -0500

    Soft boundary components are easier to make now.

--- Deep Under/Assets/AI/Scripts/SoftBoundaryComponent.cs
+++ Deep Under/Assets/AI/Scripts/SoftBoundaryComponent.cs
@@ -1,10 +1,34 @@
 ﻿using UnityEngine;
 using Extensions;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 [RequireComponent(typeof(Collider))]
+[DisallowMultipleComponent]
 public class SoftBoundaryComponent : MonoBehaviour {
 
-    [SerializeField] private SoftBoundary SoftBoundary;
+    public SoftBoundary SoftBoundary;
+
+#if UNITY_EDITOR
+    // Automatically assign the parent softboundary as the associated boundary. If non-existent, show a warning and destroy
+    void Reset() {
+        if (!this.SoftBoundary)
+        {
+            SoftBoundary parentBoundary = null;
+                if (transform.parent)
+                    { parentBoundary = transform.parent.GetComponent<SoftBoundary>(); }
+                if (parentBoundary)
+                    { this.SoftBoundary = parentBoundary; }
+                else
+                {
+                    EditorUtility.DisplayDialog("Invalid Soft Boundary Component Context", "A soft boundary component may only exist as a child of a soft boundary", "I understand");
+                    DestroyImmediate(this);
+                }
+            }
+        }
+#endif
 
 	void Start()
     {
